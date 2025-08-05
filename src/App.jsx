3d593ac8b2ba4ea9ca76f93e56e7d38e72e7771e
Mreedon <tcoudal@gmail.com>
2025-08-04 22:58:32 -0500

    🧼 Clean rollback: remove last_updated logic, finalize icon + title

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import DailySummaryLog from "./components/DailySummaryLog";
-import useMeta from "./hooks/useMeta";
 
 function App() {
-  const meta = useMeta();
-
-  function formatDate(iso) {
-    const date = new Date(iso);
-    return date.toLocaleString("en-US", {
-      month: "short",
-      day: "numeric",
-      hour: "numeric",
-      minute: "2-digit",
-      hour12: true,
-    });
-  }
-
   return (
     <div className="bg-black text-white min-h-screen font-sans [&_*]:text-white">
       <DailySummaryLog />
-      {meta && (
-        <div className="text-xs text-gray-400 text-center my-2">
-          Last updated: {formatDate(meta.last_updated)}
-        </div>
-      )}
     </div>
   );
 }
+
 
 export default App;
