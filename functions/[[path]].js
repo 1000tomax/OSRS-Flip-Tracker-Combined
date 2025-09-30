7e21b6edc685e6afc292a29afaa483e4c91ea74c
1000tomax <tcoudal@gmail.com>
2025-09-30 09:12:39 -0500

    fix: remove conflicting catch-all function for SPA routing
    Removed functions/[[path]].js which was conflicting with _middleware.js.
    This fixes 404 errors when refreshing non-home pages.

--- functions/[[path]].js
+++
@@ -1,8 +0,0 @@
-// Catch-all function for SPA routing
-// _routes.json ensures only non-asset routes reach this function
-export async function onRequest(context) {
-  const { env, request } = context;
-
-  // Serve index.html for all SPA routes
-  return env.ASSETS.fetch(new URL('/index.html', request.url));
-}
